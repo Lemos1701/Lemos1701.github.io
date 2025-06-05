4492e9b6b264772b30c84fa10f5f01697cf427f1
Vitor Lemos <vt.lemos1701@gmail.com>
2025-06-04 23:12:29 -0300

    feat: estrutura atual do site

---
+++ src/scripts.js
@@ -0,0 +1,27 @@
+const icons = document.querySelectorAll('.icon');
+
+icons.forEach(icon => {
+    const p = icon.querySelector('p');
+    let timeout;
+
+    icon.addEventListener('mouseenter', () => {
+        clearTimeout(timeout);
+        p.dataset.original = p.innerText;
+        p.style.opacity = '0';
+
+        timeout = setTimeout(() => {
+            p.innerText = "Ler";
+            p.style.opacity = '1';
+        }, 200);
+    });
+
+    icon.addEventListener('mouseleave', () => {
+        clearTimeout(timeout);
+        p.style.opacity = '0';
+
+        timeout = setTimeout(() => {
+            p.innerText = p.dataset.original;
+            p.style.opacity = '1';
+        }, 300);
+    });
+});
